61917562e24de502c98f052435121f4381db9245
theo.badoz <theo.badoz@gmail.com>
2024-09-23 15:17:54 +0200

    features: relevant KPIs and sliders

--- pages/3_📊_Comparaison.py
+++ pages/3_📊_Comparaison.py
@@ -3,8 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from utils.gcp_connector import download_dataframe
-from utils.computations import PMT, compute_remaining_capital_after_y_years
-
+import numpy_financial as npf
 st.set_page_config(page_title="📊 Comparaison", page_icon="📊", layout="wide")
 
 # Import necessary functions from Checks.py
@@ -30,17 +29,21 @@ def query_real_estate_df():
     real_estate_id = st.selectbox("Select a real estate", real_estate_df['real_estate_id'].unique())
     return real_estate_df[real_estate_df['real_estate_id'] == real_estate_id].sort_values('timestamp', ascending=False).iloc[:1].reset_index(drop=True)
 
-def create_kpi_metrics(df, year):
+def create_kpi_metrics(df, real_estate_df):
+    detention_period  = real_estate_df.loc[0, 'durée_de_détention_(année)']
+    actualisation_rate = real_estate_df.loc[0, 'taux_d_actualisation']
+    apport = real_estate_df.loc[0, 'apport']
+    st.markdown("## Key metrics after detention period")
     col1, col2, col3, col4 = st.columns(4)
-    
+
     with col1:
-        st.metric("Cumulative Cash Flow", f"€{df[f'year_{year}']['cumulative_net_cash_flow']:,.0f}")
+        st.metric("Net Cash Flow", f"€{df['cumulative_net_cash_flow'][f'year_{detention_period}']:,.0f}")
     with col2:
-        st.metric("Net Operating Income", f"€{df[f'year_{year}']['net_operating_income']:,.0f}")
+        st.metric("IRR (Internal Rate of Return)", f"{100 * npf.irr(df['net_cash_flow'].to_list()):,.2f}%")
     with col3:
-        st.metric("Property Value", f"€{df[f'year_{year}']['valeur_vénale']:,.0f}")
+        st.metric("VAN (Valeur Actuelle Nette)", f"{npf.npv(actualisation_rate, df['net_cash_flow'].to_list()):,.2f}€")
     with col4:
-        st.metric("Remaining Debt", f"€{abs(df[f'year_{year}']['capital_restant_dû']):,.0f}")
+        st.metric("EqX", f"{df['cumulative_net_cash_flow'][f'year_{detention_period}']/apport:,.2f}x")
 
 def create_cash_flow_chart(df, max_year):
     fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -93,22 +96,24 @@ def main():
     
     with col1:
         detention_period = st.slider("Durée de détention (années)", 1, 30, int(real_estate_df.loc[0, 'durée_de_détention_(année)']))
-    
-    with col2:
-        credit_rate = st.slider("Taux d'emprunt", 0.0, 5.0, 100 * float(real_estate_df.loc[0, 'taux_d_emprunt'])) / 100
         credit_duration = st.slider("Durée de crédit (années)", 1, 30, int(real_estate_df.loc[0, 'durée_de_crédit_(année)']))
+
+    with col2:
+        credit_rate = st.slider("Taux d'emprunt (en %)", 0.0, 5.0, 100 * float(real_estate_df.loc[0, 'taux_d_emprunt'])) / 100
+        actualisation_rate = st.slider("Taux d'actualisation (en %)", 0.0, 10.0, 100 * float(real_estate_df.loc[0, 'taux_d_actualisation'])) / 100
+        
     
     # Update real_estate_df with new values
     real_estate_df.loc[0, 'durée_de_détention_(année)'] = detention_period
     real_estate_df.loc[0, 'taux_d_emprunt'] = credit_rate
     real_estate_df.loc[0, 'durée_de_crédit_(année)'] = credit_duration
-    
+    real_estate_df.loc[0, 'taux_d_actualisation'] = actualisation_rate
     # Rebuild yearly_cashflow_df
     rebuilt_real_estate_df = create_additional_features(real_estate_df.copy())
     yearly_cashflow_df = build_yearly_cashflow_df(rebuilt_real_estate_df)
     df = yearly_cashflow_df.T
     
-    create_kpi_metrics(df, detention_period)
+    create_kpi_metrics(yearly_cashflow_df, rebuilt_real_estate_df)
     
     cash_flow_chart = create_cash_flow_chart(df.loc[['net_cash_flow', 'cumulative_net_cash_flow']].T, 30)
     st.plotly_chart(cash_flow_chart, use_container_width=True)
